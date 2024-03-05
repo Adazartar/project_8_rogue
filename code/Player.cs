675cbf1058a044891f27527b541ac5da67f08290
mantelloa <adam.mantello@student.unimelb.edu.au>
2024-03-05 16:35:21 +1100

    initialising

---
+++ code/Player.cs
@@ -0,0 +1,12 @@
+using Sandbox;
+
+public sealed class Player : Component
+{
+    protected override void OnStart(){
+        Log.Info("this is working");
+    }
+
+    protected override void OnUpdate(){
+        
+    }
+}
